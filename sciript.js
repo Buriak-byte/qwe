b3353ac1803ef1e8960f52f10d79ebf27efa0df9
Buriak-byte <oleksii.buriak12@gmail.com>
2026-06-19 14:40:31 +0300

    мммм

--- sciript.js
+++
@@ -1,9 +0,0 @@
-const logInBtn = document.querySelector(".MD-logIn-btn");
-const logInInput = document.querySelector(".MD-logIn-input1")
-const modalWindow = document.querySelector(".modalWindow");
-logInBtn.addEventListener("click", (logInClick) => {
-    if (logInInput.value) {
-modalWindow.style.display = "none"
-    }
-})
-console.log(modalWindow);
